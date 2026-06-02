773f080e0327ba5e57ca50ea5e3ef025e8ab9d00
kanishkzzz <kanishk8negi@gmail.com>
2026-06-03 01:34:09 +0530

    Fix CORS URL

--- server/main.py
+++ server/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import cover_letter
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://cover-letter-generator-teal.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
-  return {"message": "Server is Running!"}
+    return {"message": "Server is Running!"}
 
 app.include_router(cover_letter.router, prefix="/api")
